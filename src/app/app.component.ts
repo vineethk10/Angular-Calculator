8be2b6f2b659c9b229b180e8c81ab2bc169ea371
Vineeth Kumar Ananthula <vineethkumarananthula@gmail.com>
2022-11-15 23:18:56 -0600

    calculator-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vineethCalculator';
+  show = '0';
+  currentValue = '';
+
+  displayInput(value:string){
+    this.currentValue = this.currentValue + value;
+    this.show = this.currentValue;
+  }
+
+  getResult(){
+    this.show =  eval(this.currentValue);
+    this.currentValue = this.show;
+  }
+
+  clearData(){
+    this.currentValue = '';
+    this.show = '0';
+  }
+
+  goBack(){
+    this.currentValue = this.currentValue.slice(0,-1);
+    this.show = this.currentValue;
+    if(this.show == ''){
+      this.show = '0';
+    }
+  }
+
+  calcValue(solve:any){
+    if(solve.charAt(0) == '0'){
+      solve = solve.slice(1,);
+    }
+    this.show = eval(solve);
+  }
 }
